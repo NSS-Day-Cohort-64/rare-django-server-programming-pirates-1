1967ff78570ebac23efc72f061126160aeab9178
Brandon Highland <coffeemonsterstudio@gmail.com>
2023-08-23 10:17:09 -0500

    corrected required user profile information

--- piratesrareapi/views/auth.py
+++ piratesrareapi/views/auth.py
@@ -47,8 +47,6 @@ def register_user(request):
     )
 
     author = Author.objects.create(
-        bio=request.data['bio'],
-        profile_image_url=request.data['profile_image_url'],
         created_on=datetime.now(),
         active=True,
         user = new_user
